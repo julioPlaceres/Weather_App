8aea8ff5960addbdb91c3d2234f9b6ba247a4ed8
dreamheart <leondreamheart@outlook.com>
2021-07-04 12:35:42 -0400

    working on header, setted up bootstrap and started given it some classes, appended city name and icon to the DOM (work in progress)

---
+++ assets/js/apiKeys.js
@@ -0,0 +1,2 @@
+// API Key for the weather App. Hide on gitignore
+const apiKey = "2e370526086d5f8b1a8a2f455e36dc61"
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@
 var formEl = $("#weatherForm")
 var cityNameEl = $('input[name="city-name"]');
 var displayBtn = $("#displayBtn");
+var displayAreaEl = $("#displayResults");
 
 // Object that will storage all data for the dashboard
 var weather = {
   city: "",
+  icon: "",
   temp: "",
   wind: "",
   humidity: "",
@@ -17,7 +19,7 @@ var weather = {
 displayBtn.on("click", makeApiCalls);
 
 // Makes all api calls
-function makeApiCalls(event){
+function makeApiCalls(event) {
   event.preventDefault();
 
   getCordinates();
@@ -30,11 +32,11 @@ function getCordinates() {
   // event.preventDefault();
 
   let weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
-    + cityNameEl.val() + "&appid=" + apiKey +"&units=imperial";
+    + cityNameEl.val() + "&appid=" + apiKey + "&units=imperial";
 
   fetch(weatherApiUrl)
     .then(function (response) {
-      if(!response.ok){
+      if (!response.ok) {
         // TODO: complete this later by adding html with error handling
         console.error(response.status);
         return;
@@ -49,5 +51,40 @@ function getCordinates() {
       weather.latitude = data.coord.lat;
       weather.longitude = data.coord.lon;
       console.log(weather);
+      console.log(data);
+
+      // Create a section for the header of the data
+      var weatherHeader = $("<div>"); 
+      weatherHeader.attr("class", "row"); // Need to center this
+      weatherHeader.attr("id", "headerRow");
+
+      // Create City Name and Time element
+      var cityNameText = $("<div>");
+      // Asign it some text
+      cityNameText.text(weather.city + moment().format(" (MM/DD/YYYY) "));
+
+      // Get icon code
+      let iconCode = data.weather[0].icon;
+      // concadenate it to the url
+      let iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png"
+
+      // Create icon element
+      var weatherIcon = $("<div>");
+      // Set ID for Weather Icon
+      weatherIcon.attr("id", "icon");
+      weatherIcon.attr("display", "flex");
+      // Create Image inside the weather Icon Div
+      var imageIcon = $("<img>");
+      // Set properties for Image Icon
+      imageIcon.attr("id", "weatherIcon");
+      imageIcon.attr("src", iconUrl);
+      imageIcon.attr("alt", "Weather Icon");
+      imageIcon.attr("width", "50px");
+
+      // Append elements to the DOM
+      weatherHeader.append(cityNameText);
+      weatherHeader.append(weatherIcon);
+      weatherIcon.append(imageIcon);
+      displayAreaEl.append(weatherHeader);
     });
 }
