5c0f567a87fbf29457d5f3b0efbba1b6863a5f32
dreamheart <leondreamheart@outlook.com>
2021-07-06 23:47:45 -0400

    added error handling, refactor html and up to the first part of the API call on the javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // HTML ELemnets 
-var formEl = $("#weatherForm")
+// var formEl = $("#weatherForm")
 var cityNameEl = $('input[name="city-name"]');
 var displayBtn = $("#displayBtn");
 var displayAreaEl = $("#displayResults");
 var clearBtn = $(".clearBtn");
-var historyDiv = $("#historyBtns");
+var historyBtn = $("#historyBtns");
 var weatherHistory = [];
 
 // Object that will storage all data for the dashboard
@@ -18,20 +18,45 @@ var weather = {
   longitude: ""
 }
 
+// Event listeners
+displayBtn.on("click", makeApiCalls);  // Starts when user clicks on search 
+clearBtn.on("click", clearList);       // Starts when user click to clear search history
+historyBtn.on("click", displayTarget); // Start when user click buttons from previous buttons
 
-
+// Initiates when page loads and display the list of history searched saved
+// in the local storage
 displayMyList();
 
-// Event listeners
-displayBtn.on("click", makeApiCalls);
-clearBtn.on("click", clearList);
-historyDiv.on("click", displayTarget);
-
-function displayTarget(event){
-  var cityClicked = event.target.innerHTML;
-  getCordinates(cityClicked);
+function displayMyList() {
+  weatherHistory = JSON.parse(localStorage.getItem("cityName")) || [];
+  // Clear content to avoid duplication
+  $("#historyBtns").text("");
+  // Check if there's data saved, if there's will loop through items
+  // and call function add name to create buttons for each
+  if (weatherHistory.length > 0) {
+    for (let i = 0; i < weatherHistory.length; i++) {
+      addName(weatherHistory[i]);
+    }
+  }
 }
 
+function addName(cityName) {
+  // Creates button and adds the city name as text
+  var btn = $("<button>").text(cityName);
+  // Set some properties for styling
+  btn.attr("class", "btn btn-secondary my-2");
+  // Append button to the page
+  $("#historyBtns").append(btn);
+}
+
+// When user click on searched history buttons will trigger API calls based on 
+// the value specifed on the target text
+function displayTarget(event){
+  var cityClicked = event.target.innerHTML;
+  getCityData(cityClicked);
+}
+
+// Clear the search history
 function clearList() {
   if (weatherHistory.length > 0) {
     localStorage.removeItem("cityName");
@@ -39,50 +64,33 @@ function clearList() {
   }
 }
 
-// Makes all api calls
+// Will make the necesary API calls, sort and assign data to the proper fields 
 function makeApiCalls(event) {
   event.preventDefault();
 
-  getCordinates(cityNameEl.val());
+  getCityData(cityNameEl.val());
 }
-
-function displayMyList() {
-  weatherHistory = JSON.parse(localStorage.getItem("cityName")) || [];
-  console.log(weatherHistory);
-  $("#historyBtns").text("");
-  if (weatherHistory.length > 0) {
-    for (let i = 0; i < weatherHistory.length; i++) {
-      console.log("Index: " + i);
-      addName(weatherHistory[i]);
-    }
-  }
-}
-
-function addName(cityName) {
-  var btn = $("<button>").text(cityName);
-  btn.attr("class", "btn btn-secondary my-2");
-  $("#historyBtns").append(btn);
-  console.log(cityName);
-}
-
 
 // Retrieve some of the data based on city name 
 // also get latitued and longitude needed for the next query
-function getCordinates(searchInput) {
+function getCityData(searchInput) {
 
   let weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
     + searchInput + "&appid=" + apiKey + "&units=imperial";
 
   fetch(weatherApiUrl)
     .then(function (response) {
+      // Checks if there's any error and display to the page
       if (!response.ok) {
-        // TODO: complete this later by adding html with error handling
-        console.error(response.status);
+        var error = $(".errorHandling");
+        var errorDescription = $("<div>").text(response.status + " " + response.statusText);
+        error.append(errorDescription);
         return;
       }
       return response.json();
     })
     .then(function (data) {
+      // Assign values from response to object properties
       weather.city = data.name;
       weather.temp = data.main.temp + " °F";
       weather.wind = data.wind.speed + " MPH";
@@ -157,19 +165,19 @@ function getCordinates(searchInput) {
 
       //===========================================================================================
 
-      let weatherApiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?" +
+      let forecastApiUrl = "https://api.openweathermap.org/data/2.5/onecall?" +
         "lat=" + weather.latitude + "&lon=" + weather.longitude + "&exclude=" + "hourly" + "&units=imperial" + "&appid=" + apiKey
 
-      fetch(weatherApiUrl2)
-        .then(function (response2) {
-          if (!response2.ok) {
+      fetch(forecastApiUrl)
+        .then(function (forecastResponse) {
+          if (!forecastResponse.ok) {
             // TODO: complete this later by adding html with error handling
-            console.error(response2.status);
+            console.error(forecastResponse.status);
             return;
           }
-          return response2.json();
+          return forecastResponse.json();
         })
-        .then(function (data2) {
+        .then(function (forecastData) {
 
           // Clear all cards before appending new ones
           $(".forecast").html("");
@@ -179,29 +187,29 @@ function getCordinates(searchInput) {
           var cardRow = $("<div>").attr("class", "row text-center");
           card.append(cardRow);
 
-          $(data2.daily).each(function (element) {
+          $(forecastData.daily).each(function (element) {
             if (element == 5) { return false }
 
             // Date & Time
-            let dt = data2.daily[element].dt
+            let dt = forecastData.daily[element].dt
             dt = new Date(dt * 1000);
             dt = moment(dt, "MM/DD/YYYY").format("(MM/DD/YYYY)");
 
             // Get icon code
-            let iconCode = data2.daily[element].weather[0].icon;
+            let iconCode = forecastData.daily[element].weather[0].icon;
 
             // concadenate it to the url
             let iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
 
-            let temp2 = data2.daily[element].temp.day;
-            let wind2 = data2.daily[element].wind_speed;
-            let humidity2 = data2.daily[element].humidity;
+            let forecastTemp = forecastData.daily[element].temp.day;
+            let forecastWind = forecastData.daily[element].wind_speed;
+            let forecastHumidity = forecastData.daily[element].humidity;
 
             let cardDay = $("<div>").attr("class", "col-md card ms-3 w-auto");
             let dateEl = $("<div>").text(dt);
-            let tempEl = $("<div>").text("Temp: " + temp2);
-            let windEl = $("<div>").text("Wind: " + wind2);
-            let humidityEl = $("<div>").text("Humidity: " + humidity2);
+            let tempEl = $("<div>").text("Temp: " + forecastTemp);
+            let windEl = $("<div>").text("Wind: " + forecastWind);
+            let humidityEl = $("<div>").text("Humidity: " + forecastHumidity);
             let weatherIcon = $("<div>").attr("id", "icon");
             let imageIcon = $("<img>");
             imageIcon.attr("id", "weatherIcon");
